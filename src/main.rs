1b9f66cce5e8a437f0964a511a02a447bd0485ba
mich4ld <corollid@gmail.com>
2022-01-06 22:46:19 +0100

    add strut indexes

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use x11::xlib::{ButtonPressMask, ExposureMask};
 
 const ROOT_UID: u32 = 0;
 
+// STRUT INDEXES:
+const TOP: usize = 2;
+const BOTTOM: usize = 3;
+const TOP_START_X: usize = 8;
+const TOP_END_X: usize = 9;
+const BOTTOM_START_X: usize = 10;
+const BOTTOM_END_X: usize = 11;
+
 fn main() {
     unsafe {
         let uid = libc::getuid();
@@ -18,15 +26,15 @@ fn main() {
         let bar = x.create_window(
             screen_info.x, 
             screen_info.y, 
-            screen_info.width as u32, 
+            screen_info.width as u32 - 1, 
             bar_height, 
             0xffffff
         );
 
         let mut strut: [i64; 12] = [0; 12];
-        strut[2] = bar_height as i64;
-        strut[8] = screen_info.x as i64;
-        strut[9] = screen_info.x as i64 + screen_info.width as i64 - 1;
+        strut[TOP] = bar_height as i64;
+        strut[TOP_START_X] = screen_info.x as i64;
+        strut[TOP_END_X] = screen_info.x as i64 + screen_info.width as i64 - 1;
         
         let ewmh = Ewmh::new(&x);
         ewmh.set_strut(bar, strut);
